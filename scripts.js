bb9f2a1fa89304b69ad9320c24fd6beeb028b866
Irene <icn2104@columbia.edu>
2014-02-28 23:28:57 -0500

    I've included a for loop and fed data into my visualization library. I also used the configuration option, title to customize my chart

--- scripts.js
+++ scripts.js
@@ -16,7 +16,53 @@ function dataUp(uEMPDATA){            //uEMPDATA is the local name for the JSON 
 	arrayArray.push(arrayHeader);       //I'm inserting the headers at the top of my page
 	
 	
+	//specify starting point, ending point
+	for(i=0; i<obsData.length; i++){       //1st parameter is starting point
+		
+		//create reference to current object in list
+		var ObjCur = obsData[i];
+		var ArrayCur= [ObjCur.date, Number(ObjCur.value)];   //Number converts the curObj from strings to numbers
+		arrayArray.push(ArrayCur);                          //pushing the little arrays into the big array, arrayArray
+		
+	}// end of for loop
 	
+	console.log(arrayArray); //I'm outputting my array of arrays
+	
+	
+	var DataTable = google.visualization.arrayToDataTable(arrayArray); //I'm putting data into my visualization library
+	
+	
+	var ChartChoices = {                           //create options object to actually customize the look of our chart
+          title: "Unemployment Since 1980"       //This is a configuration option
+        };
+	//tells Google Visualization to create a line chart and give it to the
+	var ThisChart = new google.visualization.LineChart(document.getElementById('newChartDiv')); //I've created type of chart e.g. BarChart to LineChart etc
+	ThisChart.draw(DataTable,ChartChoices);
+}
+
+
+function GoogleUp() {
+
+	console.log("I've put up Google!");
+	
+		
+	$.get("UEMP270V_data.json", dataUp, "json"); //I'm retrieving my the json 
+
+}
+
+
+function docUp() {
+
+	console.log("My page is up!");
+
+	//I'm loading the google visualization library
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		callback : "GoogleUp"
+	});
+
+}
+
 
 $(document).ready(docUp);  //I've used $(document).ready to call my function, docUp
 
